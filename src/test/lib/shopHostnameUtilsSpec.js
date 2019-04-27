077466da45d1aa0ae36d3a11ae49c41dc060b819
k3v1n <k3v1n0x90@gmail.com>
2019-04-27 12:53:55 +0200

    Feature #22: Add tests for shopHostnameUtils

--- src/test/lib/shopHostnameUtilsSpec.js
+++ src/test/lib/shopHostnameUtilsSpec.js
@@ -1,12 +1,34 @@
-import { shopNameToHostname } from 'shopHostnameUtils';
+import { shopNameToHostname, findActiveShop } from 'shopHostnameUtils';
 
 describe('Provides utility methods to get the hostname from a shop name and the other way around', () => {
-
-  it('should correctly resolve a shop name to a hostname', () => {
-    const shopName = 'about you';
-
+  it.each`
+    shopName          | expectedHostname
+    ${'about you'}    | ${'aboutyou'}
+    ${'mein koffer'}  | ${'mein-koffer'}
+  `('should correctly resolve a shop name $shopName to hostname $hostname', ({shopName, expectedHostname}) => {
     const hostname = shopNameToHostname(shopName);
 
-    expect(hostname).toEqual('aboutyou');
+    expect(hostname).toEqual(expectedHostname);
+  });
+
+  it.each`
+    hostname          | expectedShop
+    ${'my-test-shop'} | ${{ shopName: 'my test shop', hostname: 'my-test-shop'}}
+    ${'unknown'}      | ${undefined}
+  `('should find the shop $shop to a hostname $hostname', ({hostname, expectedShop}) => {
+    const shops = [
+      {
+        shopName: 'my test shop',
+        hostname: 'my-test-shop'
+      },
+      {
+        shopName: 'unknown shop',
+        hostname: 'unknown-shop'
+      }
+    ]
+
+    const activeShop = findActiveShop(shops, hostname);
+
+    expect(activeShop).toEqual(expectedShop);
   });
 });
