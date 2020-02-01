f809dca512eac176565fbb5381a58cb4d22d9086
k3v1n <k3v1n0x90@gmail.com>
2020-02-01 20:48:24 +0100

    Solve format error

--- src/jest.config.integration.js
+++ src/jest.config.integration.js
@@ -30,11 +30,7 @@ module.exports = {
   resetMocks: true,
 
   // A list of paths to directories that Jest should use to search for files in
-<<<<<<< HEAD:src/jest.config.integration.js
   roots: ['<rootDir>/test/integration', '<rootDir>/app'],
-=======
-  roots: ['<rootDir>/test', '<rootDir>/app'],
->>>>>>> master:src/jest.config.js
 
   // Allows you to use a custom runner instead of Jest's default test runner
   runner: 'jest-runner',
